920106fcce60c04a311ea8469ddaad612f790425
Grace Hu <grace@scu.edu.tw>
2024-01-26 11:13:35 +0800

    modify

--- UI/main.py
+++ UI/main.py
@@ -1,7 +1,7 @@
 import requests
 import pandas as pd
 
-url = 'https://openapi-test-6vnu.onrender.com/pico_w/?count=5'
+url = 'https://open-api-q2kw.onrender.com/pico_w/?count=5'
 
 r = requests.get(url=url)
 
